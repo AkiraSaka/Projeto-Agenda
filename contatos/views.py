2b210780bb6ee5181525cad80dca633f3cff4bc6
AkiraSaka <matheussaka@gmail.com>
2022-07-06 23:45:51 -0300

    Adição de barra de busca

--- contatos/views.py
+++ contatos/views.py
@@ -2,7 +2,7 @@ from django.http import Http404
 from django.shortcuts import render, get_object_or_404
 from .models import Contato
 from django.core.paginator import Paginator
-
+from django.db.models import Q
 
 def index(request):
     contatos = Contato.objects.all()
@@ -24,4 +24,20 @@ def ver_contato(request, contato_id):
 
     return render(request, 'contatos/ver_contato.html', {
         'contato': contato
+    })
+
+def busca(request):
+    termo = request.GET.get('termo')
+
+    contatos = Contato.objects.order_by('-id').filter(
+        Q(nome__icontains = termo) | Q(sobrenome__icontains = termo),
+        mostrar = True
+    )
+    paginator = Paginator(contatos, 2)
+
+    page = request.GET.get('p') #paginação
+    contatos = paginator.get_page(page)
+
+    return render(request, 'contatos/busca.html', {
+        'contatos': contatos
     })
